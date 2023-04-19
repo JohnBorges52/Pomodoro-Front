793110b4707f62b4828a1ed8f12e2ea39fb03994
john <jrborges52@gmail.com>
2023-04-19 13:18:22 -0700

    .

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -1,48 +1,56 @@
-import axios from "axios"
-import { useEffect } from "react"
-import { useState } from "react"
-import "../styles/myprofile.scss"
-import { BigSticker } from "./BigSticker"
-import { BigStickerLocked } from "./BigStickerLocked"
+import axios from 'axios'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import '../styles/myprofile.scss'
+import { BigSticker } from './BigSticker'
+import { BigStickerLocked } from './BigStickerLocked'
 
-
-export const MyProfile = (props) => {
+export const MyProfile = props => {
   const [numberOfPomodoros, setNumberOfPomodoros] = useState()
-  
+
   const [allStickers, setAllStickers] = useState([])
   const [myStickers, setMyStickers] = useState([])
   const [currentSticker, setCurrentSticker] = useState()
-  
+
   const [openDiv, setOpenDiv] = useState(false)
   const [openLockedDiv, setOpenLockedDiv] = useState(false)
   const [loading, setLoading] = useState(true)
 
-
-  const loggedinUser = localStorage.getItem("user")
+  const loggedinUser = localStorage.getItem('user')
   const data = JSON.parse(loggedinUser)
   const userID = data.id
- 
-  
+
   const fetchNumOfPomodoros = () => {
-    axios.post("https://pomodoro-api-production.up.railway.app/pomodoros/mypomodoros", {userID})
-    .then(res => {setNumberOfPomodoros(res.data[0].exact_count)})
+    axios
+      .post(
+        'https://pomodoro-api-production.up.railway.app/pomodoros/mypomodoros',
+        { userID }
+      )
+      .then(res => {
+        setNumberOfPomodoros(res.data[0].exact_count)
+      })
   }
 
   useEffect(() => {
-    fecthAllStickers();
-    fetchNumOfPomodoros();
-  },[])
+    fecthAllStickers()
+    fetchNumOfPomodoros()
+  }, [])
 
   const fecthAllStickers = () => {
     setLoading(true)
-    axios.get("https://pomodoro-api-production.up.railway.app/stickers")
-    .then(res => { setAllStickers(res.data); setLoading(false)})
-   
+    axios
+      .get(
+        'postgres://animepomosql_user:jKDK8oEkwl1Q8JibFKbMj9sLPL7716KM@dpg-ch034hrh4hstquodonlg-a/animepomosql/stickers'
+      )
+      .then(res => {
+        setAllStickers(res.data)
+        setLoading(false)
+      })
   }
-   
-  const myStickersId = (myStickers) => {
+
+  const myStickersId = myStickers => {
     let result = []
-    myStickers.map((e)=>{
+    myStickers.map(e => {
       result.push(e.id)
     })
     return result
@@ -50,8 +58,8 @@ export const MyProfile = (props) => {
 
   const mapStickersInformation = (allStickers, currentStickerId) => {
     let result = 0
-    allStickers.map((e)=>{
-      if(currentStickerId === e.id){
+    allStickers.map(e => {
+      if (currentStickerId === e.id) {
         result = [e.stickerpic, e.title, e.id]
       }
     })
@@ -60,51 +68,98 @@ export const MyProfile = (props) => {
 
   return (
     <>
-    <div className="my-profile-main-container">
-      {openDiv && <BigSticker img={mapStickersInformation(allStickers, currentSticker)[0]} title={mapStickersInformation(allStickers, currentSticker)[1]} msg={`You have won Sticker after ${mapStickersInformation(allStickers, currentSticker)[2]} completed pomodoros. `} close={()=> setOpenDiv(false)}/>}
-      
-      {openLockedDiv && <BigStickerLocked img={mapStickersInformation(allStickers, currentSticker)[0]} title={mapStickersInformation(allStickers, currentSticker)[1]} msg={`You can win this sticker after ${mapStickersInformation(allStickers, currentSticker)[2]} completed pomodoros. `} close={()=> setOpenLockedDiv(false)}/>}
-      
-      <div className="my-profile-span-wrapper">
-      <div className="myprofile-span">
-        <span > Welcome to your page <span className="myprofile-span-bolder">{data.username}</span>. </span>
-      </div>
-     
-      <div className="myprofile-span">
-        <span className="span--little" > You have this account since {data.created_at.substring(5,10)}-{data.created_at.substring(0,4)}.</span>
-      </div>
+      <div className="my-profile-main-container">
+        {openDiv && (
+          <BigSticker
+            img={mapStickersInformation(allStickers, currentSticker)[0]}
+            title={mapStickersInformation(allStickers, currentSticker)[1]}
+            msg={`You have won Sticker after ${
+              mapStickersInformation(allStickers, currentSticker)[2]
+            } completed pomodoros. `}
+            close={() => setOpenDiv(false)}
+          />
+        )}
 
-      <div className="myprofile-span">
-        <span>You have done <span className="span--bold"> {numberOfPomodoros}</span> pomodoros so far. </span>
-      </div>
+        {openLockedDiv && (
+          <BigStickerLocked
+            img={mapStickersInformation(allStickers, currentSticker)[0]}
+            title={mapStickersInformation(allStickers, currentSticker)[1]}
+            msg={`You can win this sticker after ${
+              mapStickersInformation(allStickers, currentSticker)[2]
+            } completed pomodoros. `}
+            close={() => setOpenLockedDiv(false)}
+          />
+        )}
 
-      </div>
+        <div className="my-profile-span-wrapper">
+          <div className="myprofile-span">
+            <span>
+              {' '}
+              Welcome to your page{' '}
+              <span className="myprofile-span-bolder">
+                {data.username}
+              </span>.{' '}
+            </span>
+          </div>
+
+          <div className="myprofile-span">
+            <span className="span--little">
+              {' '}
+              You have this account since {data.created_at.substring(5, 10)}-
+              {data.created_at.substring(0, 4)}.
+            </span>
+          </div>
+
+          <div className="myprofile-span">
+            <span>
+              You have done{' '}
+              <span className="span--bold"> {numberOfPomodoros}</span> pomodoros
+              so far.{' '}
+            </span>
+          </div>
+        </div>
         <div className="my-stickers-tittle">
-        My Stickers
-
-        {/* <span>MY STICKERS</span>
+          My Stickers
+          {/* <span>MY STICKERS</span>
         <button onClick={()=>{console.log("HERE::", mapStickersInformation(allStickers, currentSticker)}}>TEST</button> */}
         </div>
         {loading && <div className="loader-container"></div>}
-        
-      <div className="my-stickers-container">
 
-        {(allStickers.map((element) => {
-         // const myIds = myStickersId(myStickers)
-          if (element.id <= numberOfPomodoros) {
-              return ( 
-              <div key={element.id} className="mytrophies" >
-                <img className="sticker-img" alt={`${element.title}`} onClick={()=> {setCurrentSticker(element.id); setOpenDiv(true)}} src={`${element.stickerpic}`} />
-              </div>)} 
-          else {
-            return ( 
-            <div key={element.id} className="mytrophies" >
-              <img className="sticker-img locked" alt={`${element.title}`} onClick={()=> {setCurrentSticker(element.id); setOpenLockedDiv(true)}}  src={`${element.stickerpic}`} />
-            </div>)}
-        }))}
+        <div className="my-stickers-container">
+          {allStickers.map(element => {
+            // const myIds = myStickersId(myStickers)
+            if (element.id <= numberOfPomodoros) {
+              return (
+                <div key={element.id} className="mytrophies">
+                  <img
+                    className="sticker-img"
+                    alt={`${element.title}`}
+                    onClick={() => {
+                      setCurrentSticker(element.id)
+                      setOpenDiv(true)
+                    }}
+                    src={`${element.stickerpic}`}
+                  />
+                </div>
+              )
+            } else {
+              return (
+                <div key={element.id} className="mytrophies">
+                  <img
+                    className="sticker-img locked"
+                    alt={`${element.title}`}
+                    onClick={() => {
+                      setCurrentSticker(element.id)
+                      setOpenLockedDiv(true)
+                    }}
+                    src={`${element.stickerpic}`}
+                  />
+                </div>
+              )
+            }
+          })}
+        </div>
       </div>
-    </div>
-    
     </>
   )
 }
